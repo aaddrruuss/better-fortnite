efb949796ba06e792cd4fd5f4a6eaaa4c0c876a3
Adrián Pérez Masar <permasadrian@gmail.com>
2025-02-22 02:52:08 +0100

    New file to create the cmd interface

---
+++ src/ui/cmd_interface.py
@@ -0,0 +1,14 @@
+
+
+def cmd_interface():
+    print("\n========================================")
+    print("Instructions and Hotkeys:")
+    print("  - AltGr+L: Executes 'Leave Party' with the current account")
+    print("  - AltGr+S: Executes 'Skip' with the current account")
+    print("  - AltGr+Up: Launches Fortnite with the current account")
+    print("  - AltGr+Right: Switches to the next account (or prompts to add a new one)")
+    print("  - AltGr+Left: Switches to the previous account")
+    print("  - AltGr+Down: Displays the current account in the console")
+    print("  - AltGr+Q: Opens FortniteDB.com in the browser")
+    print("========================================")
+    print("Press ALTGR+ESC to exit.")
