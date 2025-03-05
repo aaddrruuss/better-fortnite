5606ee685ec59e819e6ecc312cdf054c3b29fa32
Adrian Perez Masar <permasadrian@gmail.com>
2025-03-05 02:03:12 +0100

    Update Epic Games API credentials in configuration

--- src/config.py
+++ src/config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-EPIC_CLIENT_ID_SWITCH = os.getenv("EPIC_CLIENT_ID_SWITCH")
-EPIC_CLIENT_SECRET_SWITCH = os.getenv("EPIC_CLIENT_SECRET_SWITCH")
-EPIC_PC_CLIENT_ID = os.getenv("EPIC_PC_CLIENT_ID")
-EPIC_PC_CLIENT_SECRET = os.getenv("EPIC_PC_CLIENT_SECRET")
+EPIC_PC_CLIENT_ID="34a02cf8f4414e29b15921876da36f9a"
+EPIC_PC_CLIENT_SECRET="daafbccc737745039dffe53d94fc76cf"
+EPIC_CLIENT_ID_SWITCH="98f7e42c2e3a4f86a74eb43fbb41ed39"
+EPIC_CLIENT_SECRET_SWITCH="0a2449a2-001a-451e-afec-3e812901c4d7"
 
 OAUTH_TOKEN_URL = "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token"
 DEVICE_CODE_URL = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/deviceAuthorization"
